6b173037f6602d53b1c1e902c9e26daeb04cef1d
Serge Sozonoff <serge@sozonoff.com>
2019-03-14 09:35:14 +0100

    First Working example

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,27 +1,20 @@
 package com.example.demo;
 
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.classify.SubclassClassifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
 import org.springframework.kafka.support.converter.MessagingMessageConverter;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
-import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.retry.RetryContext;
 import org.springframework.retry.RetryPolicy;
-import org.springframework.retry.RetryState;
 import org.springframework.retry.backoff.ExponentialBackOffPolicy;
 import org.springframework.retry.policy.AlwaysRetryPolicy;
 import org.springframework.retry.policy.ExceptionClassifierRetryPolicy;
@@ -30,37 +23,31 @@ import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
-@EnableKafka
-@EmbeddedKafka
 public class DemoApplication {
-
-    Logger log = LoggerFactory.getLogger(this.getClass());
-
-    @Bean
-    public EmbeddedKafkaBroker kafkaBroker() {
-        EmbeddedKafkaBroker broker = new EmbeddedKafkaBroker(1, true);
-        broker.kafkaPorts(9092);
-        return broker;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
     @Bean
+    public EmbeddedKafkaBroker kafkaBroker() {
+        EmbeddedKafkaBroker broker = new EmbeddedKafkaBroker(1, false);
+        broker.kafkaPorts(9092);
+        return broker;
+    }
+
+    @Bean
     public NewTopic topic() {
-        return new NewTopic("kgh968", 2, (short) 1);
+        return new NewTopic("demo", 3, (short) 1);
     }
 
     @Bean
     public ApplicationRunner runner(KafkaTemplate<String, String> template) {
-        return args -> {
-            IntStream.range(0, 2).forEach(i -> template.send("kgh968", 0, null, String.valueOf(i)));
-        };
+        return args -> IntStream.range(0, 3).forEach(i -> template.send("demo", i, null, String.valueOf(i)));
     }
 
     @Bean
@@ -68,64 +55,76 @@ public class DemoApplication {
             ConsumerFactory<String, String> consumerFactory) {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
-
         factory.setConcurrency(2);
+
         factory.getContainerProperties().setAckOnError(false);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.RECORD);
+
         factory.setMessageConverter(new MessagingMessageConverter());
         factory.setStatefulRetry(true);
 
-        // communicate between the retrytemplate and the seeking error handler
-        ThreadLocal<Boolean> shouldSeek = ThreadLocal.withInitial(() -> true);
-        factory.setErrorHandler(new SeekToCurrentErrorHandler(-1) {
-            @Override
-            public void handle(Exception thrownException, List<ConsumerRecord<?, ?>> records,
-                               Consumer<?, ?> consumer, MessageListenerContainer container) {
-                if (shouldSeek.get()) {
-                    super.handle(thrownException, records, consumer, container);
-                }
-            }
-        });
+        // error handler
+        factory.setErrorHandler(new SeekToCurrentErrorHandler(-1));
 
-        RetryTemplate retryTemplate = new RetryTemplate() {
-            @Override
-            protected boolean shouldRethrow(RetryPolicy retryPolicy, RetryContext context, RetryState state) {
-                shouldSeek.set(retryPolicy.canRetry(context));
-                return true;
-            }
-        };
+        RetryTemplate retryTemplate = new RetryTemplate();
 
-
+        // retry policy
         ExceptionClassifierRetryPolicy policy = new ExceptionClassifierRetryPolicy();
-        policy.setExceptionClassifier(
-                new UnrwappingSubclassClassifier(
-                        new HashMap<Class<? extends Throwable>, RetryPolicy>() {
-                            {
-                                put(ExceptionA.class, new AlwaysRetryPolicy());
-                                put(ExceptionB.class, new SimpleRetryPolicy(5));
-                            }
-                        },
-                        new NeverRetryPolicy()
-                ));
-
+        Map<Class<? extends Throwable>, RetryPolicy> throwableRetryPolicyMap = new HashMap<>();
+        // A is always retried
+        throwableRetryPolicyMap.put(ExceptionA.class, new AlwaysRetryPolicy());
+        // B is retried 5 times
+        throwableRetryPolicyMap.put(ExceptionB.class, new SimpleRetryPolicy(5));
+        // the rest is never retied
+        policy.setExceptionClassifier(new UnrwappingSubclassClassifier(throwableRetryPolicyMap, new NeverRetryPolicy()));
 
         retryTemplate.setRetryPolicy(policy);
         retryTemplate.setBackOffPolicy(new ExponentialBackOffPolicy());
-
         factory.setRetryTemplate(retryTemplate);
+
+        factory.setRecoveryCallback(context -> null);
 
         return factory;
     }
 
-    @KafkaListener(topics = "kgh968",
-            groupId = "kgh968",
+    @KafkaListener(topics = "demo",
+            groupId = "demo",
             properties = {"max.poll.interval.ms:120000"})
     public void listen(String in) {
-        log.debug(in);
-        System.err.println("----------" + in);
-        if (in.equalsIgnoreCase("1"))
-            throw new ExceptionA("------> " + in);
-        else
-            throw new ExceptionB("------> " + in);
-
+        System.out.println("New incoming message: " + in);
+        if ("0".equals(in)) {
+            throw new ExceptionA();
+        } else if ("1".equals(in)) {
+            throw new ExceptionB();
+        } else if ("2".equals(in)) {
+            throw new ExceptionC();
+        }
     }
+
+    static class UnrwappingSubclassClassifier extends SubclassClassifier<Throwable, RetryPolicy> {
+
+        public UnrwappingSubclassClassifier(Map<Class<? extends Throwable>, RetryPolicy> typeMap, RetryPolicy defaultValue) {
+            super(typeMap, defaultValue);
+        }
+
+        @Override
+        public RetryPolicy classify(Throwable classifiable) {
+            Throwable cause = classifiable;
+            while (cause != null) {
+                if (getClassified().containsKey(cause.getClass())) {
+                    return super.classify(cause);
+                }
+                cause = cause.getCause();
+            }
+            return getDefault();
+        }
+    }
+
+    static class ExceptionA extends RuntimeException {
+    }
+    static class ExceptionB extends RuntimeException {
+    }
+    static class ExceptionC extends RuntimeException {
+    }
+
 }
